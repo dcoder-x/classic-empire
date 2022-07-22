4caac28993d91e816cafae57ea68ba720ca7a53b
dcoder-x <ayomikunfaluyi@gmail.com>
2022-07-22 13:31:13 +0100

    mobile responsive

--- scripts/anim.js
+++ scripts/anim.js
@@ -25,30 +25,41 @@ figure = document.querySelector('figure'),
 slideLeftBtn = document.querySelector('.left'),
 slideRightBtn = document.querySelector('.right')
 
-let width = figure.offsetWidth;
-console.log(figure.offsetWidth)
-slideLeftBtn.onclick =()=>{
-    console.log(figure.offsetWidth)
-    figure.classList.remove('slide')
-    setTimeout(()=>{
-        figure.classList.add('slide')
-        console.log('added')
-    },5000)
-    clearTimeout()
-    figure.scrollBy(-(width-100),0)
-    console.log('hello')
+// let width = figure.offsetWidth;
+// console.log(figure.offsetWidth)
+// slideLeftBtn.onclick =()=>{
+//     console.log(figure.offsetWidth)
+//     figure.classList.remove('slide')
+//     setTimeout(()=>{
+//         figure.classList.add('slide')
+//         console.log('added')
+//     },5000)
+//     clearTimeout()
+//     figure.scrollBy(-(width-100),0)
+//     console.log('hello')
    
     
-}
-slideRightBtn.onclick =()=>{
-    console.log(figure.offsetWidth)
-    figure.classList.remove('slide')
-    setTimeout(()=>{
-        figure.classList.add('slide')
-        console.log('added')
-    },5000)
-    clearTimeout()
-    figure.scrollBy((width),width)
-    console.log('hello')
+// }
+// slideRightBtn.onclick =()=>{
+//     console.log(figure.offsetWidth)
+//     figure.classList.remove('slide')
+//     setTimeout(()=>{
+//         figure.classList.add('slide')
+//         console.log('added')
+//     },5000)
+//     clearTimeout()
+//     figure.scrollBy((width),width)
+//     console.log('hello')
     
+// }
+
+const nav = document.querySelector('nav')
+window.onscroll=()=>{
+    if (document.body.scrollTop||document.documentElement.scrollTop>100) {
+        nav.style.backgroundColor="rgba(255, 255, 255, 0.433)"
+    } else {
+        nav.style.backgroundColor="rgba(0, 0, 0, 0)"
+    }
 }
+
+const sliderText = document.querySelector('.text')
