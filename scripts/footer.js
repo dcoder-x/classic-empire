12a77228823a8dfa8ad6e6a2dcbe8f9e40e7e19b
dcoder-x <ayomikunfaluyi@gmail.com>
2022-10-03 02:00:46 +0100

    updated

--- scripts/footer.js
+++ scripts/footer.js
@@ -10,10 +10,19 @@ rel="stylesheet"
     © 2022 Classic Empire Records, All rights reserved. 
 </small>
 <div class="socials" >
-    <span class="iconify" data-aos="fade-up" data-icon="ic:baseline-email" style="color: #f58632;"></span>
+    <a href=''>
+        <span class="iconify" data-aos="fade-up" data-icon="ic:baseline-email" style="color: #f58632;"></span>
+    </a>
+    <a href=''>
     <span class="iconify" data-aos="fade-up" data-aos-delay='100'  data-icon="entypo-social:facebook-with-circle" style="color: #f58632;"></span>
+    </a>
+    <a href=''>
     <span class="iconify" data-aos="fade-up" data-aos-delay='200' data-icon="ant-design:twitter-circle-filled" style="color: #f58632;"></span>
+    </a>
+    <a href='https://www.instagram.com/classic.empire_/'>
     <span class="iconify" data-aos="fade-up" data-aos-delay='300' data-icon="bxl:instagram-alt" style="color: #f58632;"></span>
+    </a>
+    
 </div>
 </footer>
 `;
